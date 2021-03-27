ff8f6eb4a4909514da5cf1a94191053c816a40c8
marysol <marilirulita@gmail.com>
2021-03-27 05:43:11 -0700

    Add answers questions and test 2

--- answer.sql
+++ answer.sql
@@ -1,4 +1,3 @@
-
 SELECT population FROM world WHERE name = 'Germany';
 SELECT name, population FROM world WHERE name IN ('Sweden', 'Norway', 'Denmark');
 SELECT name, area FROM world WHERE area BETWEEN 200000 AND 250000;
@@ -27,3 +26,26 @@ SELECT name FROM world WHERE capital like '%City';
 SELECT capital, name FROM world WHERE capital LIKE CONCAT('%', name, '%');
 SELECT capital, name FROM world WHERE capital LIKE CONCAT(name, '_%');
 SELECT name, REPLACE(capital, name, '') AS ext FROM world WHERE capital LIKE CONCAT(name, '_%');
+
+
+SELECT name, continent, population FROM world;
+SELECT name FROM world WHERE population > 200000000;
+SELECT name, GDP/population FROM world WHERE population > 200000000;
+SELECT name, population/1000000 FROM world WHERE continent = 'South America';
+SELECT name, population FROM world WHERE name IN ('France', 'Germany', 'Italy');
+SELECT name FROM world WHERE name LIKE '%United%';
+SELECT name, population, area FROM world WHERE area > 3000000 OR population > 250000000;
+SELECT name, population, area FROM world WHERE area > 3000000 XOR population > 250000000;
+SELECT name, ROUND(population/1000000, 2), ROUND(gdp/1000000000, 2) FROM world WHERE continent = 'South America';
+SELECT name, ROUND(gdp/population, -3) FROM world WHERE gdp > 1000000000000;
+SELECT name, capital FROM world WHERE LENGTH(name) = LENGTH(capital);
+SELECT name, capital FROM world WHERE LEFT(name, 1) = LEFT(capital, 1) AND name <> capital;
+SELECT name FROM world WHERE name LIKE '%a%' AND name LIKE '%e%' AND name LIKE '%i%' AND name LIKE '%o%' AND name LIKE '%u%' AND name NOT LIKE '% %';
+
+SELECT name FROM world WHERE name LIKE 'U%';
+SELECT population FROM world WHERE name = 'United Kingdom';
+SELECT continent FROM world WHERE name = 'France';
+SELECT name, population / 10 FROM world WHERE population < 10000;
+SELECT name, population FROM world WHERE continent IN ('Europe', 'Asia');
+SELECT name FROM world WHERE name IN ('Cuba', 'Togo');
+SELECT name FROM world WHERE continent = 'South America' AND population > 40000000;
