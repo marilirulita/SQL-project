c92888c2f3b7b96902b882592dc567d8c2b4ea07
marysol <marilirulita@gmail.com>
2021-03-27 03:22:33 -0700

    Add answers questions and test 0

---
+++ answer.sql
@@ -0,0 +1,12 @@
+
+SELECT population FROM world WHERE name = 'Germany';
+SELECT name, population FROM world WHERE name IN ('Sweden', 'Norway', 'Denmark');
+SELECT name, area FROM world WHERE area BETWEEN 200000 AND 250000;
+
+SELECT name, population FROM world WHERE population BETWEEN 1000000 AND 1250000;
+SELECT name, population FROM world WHERE name LIKE 'Al%';
+SELECT name FROM world WHERE name LIKE '%a' OR name LIKE '%l';
+SELECT name,length(name) FROM world WHERE length(name)=5 and continent='Europe';
+SELECT name, area*2 FROM world WHERE population = 64000;
+SELECT name, area, population FROM world WHERE area > 50000 AND population < 10000000;
+SELECT name, population/area FROM world WHERE name IN ('China', 'Nigeria', 'France', 'Australia');
